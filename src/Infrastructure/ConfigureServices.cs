40be22a3c8a062ecf2512de927fad7f77801cd95
Khaled Gadhami <khaled@gdhami.net>
2023-04-17 08:58:12 +0100

    Minor cleanup

--- src/Infrastructure/ConfigureServices.cs
+++ src/Infrastructure/ConfigureServices.cs
@@ -2,14 +2,10 @@
 using HotelServices.Domain.Interfaces;
 using HotelServices.Infrastructure.Persistence.Configuration;
 using HotelServices.Infrastructure.Persistence.Repositories;
-using HotelServices.Infrastructure.Persistence.Services;
 
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization;
-using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver;
 
 namespace Microsoft.Extensions.DependencyInjection;
@@ -26,9 +22,6 @@ public static class ConfigureServices
 
     private static void ConfigureDB(this IServiceCollection services, IConfiguration configuration)
     {
-        // Configure the mapping for AdditionalService
-        //ConfigureMongoDbId();
-
         var mongoDbSettings = configuration.GetSection("MongoDbSettings");
         services.Configure<MongoDbSettings>(options =>
         {
@@ -49,53 +42,6 @@ public static class ConfigureServices
 
             return client.GetDatabase(settings.DatabaseName);
         });
-    }
-
-    private static void ConfigureMongoDbId()
-    {
-        BsonClassMap.RegisterClassMap<AdditionalService>(cm =>
-        {
-            cm.MapIdProperty(c => c.Id)
-              .SetSerializer(new Int32Serializer(BsonType.Int32));
-            cm.MapProperty(c => c.Name);
-            cm.MapProperty(c => c.Description);
-            cm.MapProperty(c => c.Price);
-            cm.MapProperty(c => c.Bookings);
-            cm.MapProperty(c => c.Images);
-
-            //cm.AutoMap();
-            //cm.MapIdProperty(c => c.Id)
-            //  .SetElementName("_id");
-            // cm.MapIdMember(c => c.Id);
-            //.SetIdGenerator(new Int32IdGenerator())
-            // .SetSerializer(new Int32Serializer(BsonType.Int32));
-        });
-
-        //BsonClassMap.RegisterClassMap<Booking>(cm =>
-        //{
-        //    cm.AutoMap();
-        //    cm.MapIdMember(c => c.Id)
-        //      .SetIdGenerator(new Int32IdGenerator())
-        //      .SetSerializer(new Int32Serializer(BsonType.Int32));
-        //});
-
-        //BsonClassMap.RegisterClassMap<Booking>(cm =>
-        //{
-        //    cm.AutoMap();
-        //    cm.MapIdProperty(c => c.Id)
-        //      .SetElementName("_id");
-        //      //.SetIdGenerator(new Int32IdGenerator())
-        //      // .SetSerializer(new Int32Serializer(BsonType.Int32));
-        //});
-
-        //BsonClassMap.RegisterClassMap<Image>(cm =>
-        //{
-        //    cm.AutoMap();
-        //    cm.MapIdMember(c => c.Id)
-        //      .SetElementName("_id");
-        //      //.SetIdGenerator(new Int32IdGenerator())
-        //      // .SetSerializer(new Int32Serializer(BsonType.Int32));
-        //});
     }
 
     private static void SetupRepositoryDI(this IServiceCollection services)
